99c9088d66736a0cd7589c7319560b4e9e32a9e5
jupiter <pengjupiter5@gmail.com>
2024-04-20 17:57:43 -0500

    image command

--- cogs/edit.py
+++ cogs/edit.py
@@ -1,6 +1,11 @@
 import logging
+import tempfile
+import io
 
 import interactions
+import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
 
 import util
 
@@ -15,3 +20,44 @@ class Edit(interactions.Extension):
     @interactions.slash_command(** util.not_implemented_args, name="edit")
     async def edit(self, ctx: interactions.SlashContext) -> None:
         await util.not_implemented(ctx)
+
+
+def from_file(file: io.IOBase) -> list[PIL.Image.Image]:
+    img = PIL.Image.open(file)
+    imgs = []
+    for i in range(img.n_frames):
+        img.seek(i)
+        imgs.append(img.convert())
+    return imgs
+
+
+def to_file(imgs: list[PIL.Image.Image]) -> tuple[tempfile._TemporaryFileWrapper, str]:
+    f = tempfile.TemporaryFile()
+    ext = ""
+    if (len(imgs) == 1):
+        ext = "png"
+    else:
+        ext = "gif"
+    imgs[0].save(f.file, ext, save_all=True, append_images=imgs[1:])
+    f.seek(0)
+    return (f, ext)
+
+
+def add_caption(imgs: list[PIL.Image.Image], text: str, relative_font_size: float = 1) -> list[PIL.Image.Image]:
+    text = text.replace(r"\n", "\n")
+    text = text.replace(r"\\", "\\")
+    texts = text.split(",")
+    if (len(texts) < 2):
+        texts.append("")
+    texts[0] = texts[0].replace(r"\,", ",")
+    texts[1] = texts[1].replace(r"\,", ",")
+    for img in imgs:
+        font_size = int(img.width / 15 * relative_font_size)
+        font = PIL.ImageFont.truetype("impact.ttf", font_size)
+        stroke_width = int(font_size/10)
+        draw = PIL.ImageDraw.Draw(img)
+        _, _, w, _ = draw.textbbox((0, 0), text, font, align="center", stroke_width=3)
+        draw.text(((img.width-w)/2, 0), texts[0], fill="white", font=font, align="center", stroke_width=stroke_width, stroke_fill="black")
+        _, _, w, h = draw.textbbox((0, 0), text, font, align="center", stroke_width=3)
+        draw.text(((img.width-w)/2, img.height-h), texts[1], fill="white", font=font, align="center", stroke_width=stroke_width, stroke_fill="black")
+    return imgs
--- cogs/image.py
+++ cogs/image.py
@@ -1,17 +1,33 @@
 import logging
+import os
+import io
+import typing
 
 import interactions
 
 import util
+from . import edit
 
 logger = logging.getLogger(__name__)
 
 
-# TODO
 class Image(interactions.Extension):
     def __init__(self, bot) -> None:
         logger.info("init")
 
-    @interactions.slash_command(** util.not_implemented_args, name="image")
-    async def image(self, ctx: interactions.SlashContext) -> None:
-        await util.not_implemented(ctx)
+    @interactions.slash_command(** util.command_args, name="image", description="send an image")
+    async def image(self, ctx: interactions.SlashContext,
+                    image: interactions.slash_str_option("the image file", True,  # type: ignore
+                                                         choices=util.as_choices(os.listdir(os.path.join("cogs", "images")))),
+                    caption: interactions.slash_str_option("text to put on the image, escaped with '\\' as necessary") = None,  # type: ignore
+                    font_size: interactions.slash_float_option("relative size of caption") = 1,  # type: ignore
+                    ) -> None:
+        await ctx.defer()
+        filename = os.path.join("cogs", "images", image)
+        if (caption == None):
+            await ctx.send(file=filename)
+        else:
+            imgs = edit.from_file(open(filename, "rb"))
+            imgs = edit.add_caption(imgs, caption, font_size)
+            f, ext = edit.to_file(imgs)
+            await ctx.send(file=interactions.File(typing.cast(io.IOBase, f.file), f"image.{ext}"))
