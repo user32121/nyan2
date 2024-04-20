b2b0d67b155b379e46e4c91ef34350327032bfe1
jupiter <pengjupiter5@gmail.com>
2024-04-20 10:03:22 -0500

    adjusted search stats

--- cogs/search.py
+++ cogs/search.py
@@ -9,8 +9,6 @@ from . import scan
 logger = logging.getLogger(__name__)
 
 
-
-
 class Search(interactions.Extension):
     def __init__(self, bot) -> None:
         logger.info("init")
@@ -30,24 +28,26 @@ class Search(interactions.Extension):
         else:
             def matches(s: str) -> int:
                 return s.lower().count(query.lower())
+        match_counts: dict[int, int] = {}
         counts: dict[int, int] = {}
         for message_id, author_id, content in scan.message_cache[ctx.channel_id]:
-            count = matches(content)
-            if (reply and count):
+            match_count = matches(content)
+            if (reply and match_count):
                 m = await ctx.channel.fetch_message(message_id)
-                if(m != None):
+                if (m != None):
                     reply = False
                     await m.reply("found")
             if (count_by == "messages"):
-                count = int(bool(count))
-            if (count):
-                counts[author_id] = counts.get(author_id, 0) + count
-        sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-        output = f"total matches: {sum(counts.values())}"
-        for author_id, count in sorted_counts:
+                match_count = int(bool(match_count))
+            if (match_count):
+                match_counts[author_id] = match_counts.get(author_id, 0) + match_count
+            counts[author_id] = counts.get(author_id, 0) + 1
+        sorted_counts = sorted(match_counts.items(), key=lambda x: x[1], reverse=True)
+        output = f"total matches: {sum(match_counts.values())}/{sum(counts.values())} ({round(sum(match_counts.values())/sum(counts.values()), 3)})"
+        for author_id, match_count in sorted_counts:
             user = await ctx.bot.fetch_user(author_id)
             if (user == None):
-                output += f"\n{user}: {count}"
+                output += f"\n{user}: {match_count}/{counts[author_id]} ({round(match_count/counts[author_id], 3)})"
             else:
-                output += f"\n{user.display_name}: {count}"
+                output += f"\n{user.display_name}: {match_count}/{counts[author_id]} ({round(match_count/counts[author_id], 3)})"
         await ctx.send(output)
