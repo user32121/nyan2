c40f9f1a83572c5a230f51169855a31ac5ea74b6
jupiter <pengjupiter5@gmail.com>
2024-05-18 19:45:26 -0700

    change coordinate description

--- cogs/edit.py
+++ cogs/edit.py
@@ -219,8 +219,8 @@ class Edit(interactions.Extension):
                    delay: typing.Annotated[int, interactions.slash_int_option("delay between frames if one is not already present, in milliseconds", min_value=0)] = 50,
                    frames: typing.Annotated[int, interactions.slash_int_option("number of frames to create if input is a static image", min_value=1)] = 10,
                    amount: typing.Annotated[float, interactions.slash_float_option("strength")] = 2,
-                   center_x: typing.Annotated[float, interactions.slash_float_option("normalized to [-1,1]")] = 0,
-                   center_y: typing.Annotated[float, interactions.slash_float_option("normalized to [-1,1]")] = 0,
+                   center_x: typing.Annotated[float, interactions.slash_float_option("in the range [-1, 1]")] = 0,
+                   center_y: typing.Annotated[float, interactions.slash_float_option("in the range [-1, 1]")] = 0,
                    ) -> None:
         await util.preprocess(ctx)
         img = image_io.from_url(file.proxy_url)
@@ -266,8 +266,8 @@ class Edit(interactions.Extension):
                    frames: typing.Annotated[int, interactions.slash_int_option("number of frames to create if input is a static image", min_value=1)] = 5,
                    cycles: typing.Annotated[float, interactions.slash_float_option("number of cycles per gif loop")] = 1,
                    radius: typing.Annotated[float, interactions.slash_float_option("strength of the offset, normalized")] = 0.5,
-                   center_x: typing.Annotated[float, interactions.slash_float_option("normalized to [-1,1]")] = 0,
-                   center_y: typing.Annotated[float, interactions.slash_float_option("normalized to [-1,1]")] = 0,
+                   center_x: typing.Annotated[float, interactions.slash_float_option("in the range [-1, 1]")] = 0,
+                   center_y: typing.Annotated[float, interactions.slash_float_option("in the range [-1, 1]")] = 0,
                    ) -> None:
         await util.preprocess(ctx)
         img = image_io.from_url(file.proxy_url)
@@ -338,8 +338,8 @@ class Edit(interactions.Extension):
     async def bulge(self, ctx: interactions.SlashContext,
                     file: file_option,
                     amount: typing.Annotated[float, interactions.slash_float_option("strength")] = 1,
-                    center_x: typing.Annotated[float, interactions.slash_float_option("normalized to [-1,1]")] = 0,
-                    center_y: typing.Annotated[float, interactions.slash_float_option("normalized to [-1,1]")] = 0,
+                    center_x: typing.Annotated[float, interactions.slash_float_option("in the range [-1, 1]")] = 0,
+                    center_y: typing.Annotated[float, interactions.slash_float_option("in the range [-1, 1]")] = 0,
                     ) -> None:
         await util.preprocess(ctx)
         img = image_io.from_url(file.proxy_url)
