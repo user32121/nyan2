b89de71de6624fbc75acebc35a5973a8c8034122
jupiter <pengjupiter5@gmail.com>
2024-11-01 21:40:26 -0500

    add /timezone now add relative time

--- cogs/timezone.py
+++ cogs/timezone.py
@@ -5,7 +5,7 @@ import warnings
 import dateutil.parser
 import dateutil.parser._parser
 import interactions
-
+import datetime
 import util
 
 from .timezones import tz
@@ -19,17 +19,20 @@ class Timezone(interactions.Extension):
 
     @interactions.slash_command(** util.command_args, name="timezone", description="get a global timestamp for a datetime")
     async def timezone(self, ctx: interactions.SlashContext,
-                       datetime: typing.Annotated[str, interactions.slash_str_option(description="preferred format: mm/dd/yyyy hh:mm:ss MM ZZ", required=True)],
+                       time: typing.Annotated[str, interactions.slash_str_option(description="preferred format: mm/dd/yyyy hh:mm:ss MM ZZ", required=True)],
                        ) -> None:
         await util.preprocess(ctx)
         msg = ""
-        try:
-            warnings.filterwarnings("error", module="dateutil.parser._parser")
-            dt = dateutil.parser.parse(datetime, tzinfos=tz.timezone_info)
-        except dateutil.parser._parser.UnknownTimezoneWarning as e:
-            msg = f"warning: {type(e)}\n"
-            warnings.resetwarnings()
-            dt = dateutil.parser.parse(datetime, tzinfos=tz.timezone_info)
-        finally:
-            warnings.resetwarnings()
-        await ctx.send(f"{msg}{interactions.Timestamp.fromdatetime(dt)}")
+        if (time == "now"):
+            dt = datetime.datetime.now()
+        else:
+            try:
+                warnings.filterwarnings("error", module="dateutil.parser._parser")
+                dt = dateutil.parser.parse(time, tzinfos=tz.timezone_info)
+            except dateutil.parser._parser.UnknownTimezoneWarning as e:
+                msg = f"warning: {type(e)}\n"
+                warnings.resetwarnings()
+                dt = dateutil.parser.parse(time, tzinfos=tz.timezone_info)
+            finally:
+                warnings.resetwarnings()
+        await ctx.send(f"{msg}{interactions.Timestamp.fromdatetime(dt)}({interactions.Timestamp.fromdatetime(dt).format(interactions.TimestampStyles.RelativeTime)})")
