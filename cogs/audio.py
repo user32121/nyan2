bbdcd1528424812abfb1555168c678183695bce3
jupiter <pengjupiter5@gmail.com>
2025-02-28 16:13:28 -0600

    queue system this is mainly just putting things in order and having a mutex to ensure tasks don't step on each others' toes

--- cogs/audio.py
+++ cogs/audio.py
@@ -15,26 +15,48 @@ import util
 logger = logging.getLogger(__name__)
 
 
-class Queue:
-    def __init__(self) -> None:
-        self.queue = []
-
-    def add(self, file: str, msg: interactions.Message) -> None:
-        self.queue.append((file, msg))
-
-    def run(self) -> None:
-        pass
-
-
 class ProgressHook:
     def progress_hook(self, info):
+        # TODO status update
         self.filename = info["filename"]
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.queue: list[tuple[str, util.PsuedoContext, interactions.TYPE_VOICE_CHANNEL]] = []
+        self.run_lock = asyncio.Lock()
+
+    async def add(self, url: str, ctx: util.PsuedoContext, channel: interactions.TYPE_VOICE_CHANNEL) -> None:
+        await ctx.edit(content="downloading...")
+        os.makedirs("audio", exist_ok=True)
+        filename = "".join(random.choices(string.ascii_letters + string.digits, k=32))
+        hook = ProgressHook()
+        ytdl = youtube_dl.YoutubeDL({"outtmpl": f"audio/{filename}.%(ext)s", "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}], "progress_hooks": [hook.progress_hook]})
+        await asyncio.to_thread(ytdl.download, [url])
+        filename = re.sub(r"(?:\.\w+)+", ".mp3", hook.filename)
+        self.queue.append((filename, ctx, channel))
+        await ctx.edit(content="queued")
+
+    async def run(self) -> None:
+        async with self.run_lock:
+            filename, ctx, channel = self.queue.pop(0)
+
+            if not channel.voice_state:
+                await channel.connect()
+
+            audio = interactions.api.voice.audio.AudioVolume(filename)
+            if not isinstance(channel.voice_state, interactions.ActiveVoiceState):
+                raise interactions.errors.BadArgument("lost access to voice channel")
+            await channel.voice_state.play(audio)
+            # TODO remove file when done
+            if not len(self.queue):
+                await channel.disconnect()
 
 
 class Audio(interactions.Extension):
     def __init__(self, bot) -> None:
         logger.info("init")
-        self.queue = []
+        self.queue = Queue()
 
     @util.store_command("url")
     @interactions.slash_command(** util.command_args, name="audio", description="voice channel audio commands", sub_cmd_name="play", sub_cmd_description="play audio from a YT url")
@@ -42,31 +64,16 @@ class Audio(interactions.Extension):
                    url: typing.Annotated[str, interactions.slash_str_option("link to the video", required=True)],
                    ) -> None:
         await util.preprocess(ctx)
-
-        if not ctx.voice_state:
-            if not isinstance(ctx.author, interactions.Member):
-                raise interactions.errors.BadArgument("command must be run in a guild channel")
-            if not isinstance(ctx.author.voice, interactions.VoiceState):
-                raise interactions.errors.BadArgument("please join a voice channel first")
-            await ctx.author.voice.channel.connect()
-
         ctx2 = util.PsuedoContext(ctx)
-        await ctx2.send(content="downloading...")
-        hook = ProgressHook()
-        os.makedirs("audio", exist_ok=True)
-        name = "".join(random.choices(string.ascii_letters + string.digits, k=32))
-        ytdl = youtube_dl.YoutubeDL({"outtmpl": f"audio/{name}.%(ext)s", "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}], "progress_hooks": [hook.progress_hook]})
-        ytdl.download([url])
 
-        filename = re.sub(r"(?:\.\w+)+", ".mp3", hook.filename)
-        audio = interactions.api.voice.audio.AudioVolume(filename)
-        if not isinstance(ctx2.ctx.guild, interactions.Guild) or not isinstance(ctx2.ctx.guild.voice_state, interactions.ActiveVoiceState):
-            raise interactions.errors.BadArgument("lost access to voice channel")
-        if ctx2.ctx.guild.voice_state.playing:
-            await ctx2.edit(content="waiting for previous audio to finish...")
-            await ctx2.ctx.guild.voice_state.wait_for_stopped()
-        await ctx2.edit(content="playing...")
-        await ctx2.ctx.guild.voice_state.play(audio)
+        if not isinstance(ctx.author, interactions.Member):
+            raise interactions.errors.BadArgument("command must be run in a guild channel")
+        if not isinstance(ctx.author.voice, interactions.VoiceState):
+            raise interactions.errors.BadArgument("please join a voice channel first")
+        channel = ctx.author.voice.channel
+
+        await self.queue.add(url, ctx2, channel)
+        await self.queue.run()
 
     @util.store_command()
     @interactions.slash_command(** util.command_args, name="audio", description="voice channel audio commands", sub_cmd_name="skip", sub_cmd_description="stop the current audio")
