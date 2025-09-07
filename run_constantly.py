dcd4c2ccec58ce3465b534da9932961d202480d6
Juniper <pengjupiter5@gmail.com>
2025-09-07 09:58:41 -0500

    update script to autoupdate packages and use python path

--- run_constantly.py
+++ run_constantly.py
@@ -1,9 +1,14 @@
 import os
+import sys
+
+PYTHON = sys.executable
 
 with open('autorestart.txt', 'w') as f:
     f.write('delete this file to stop auto restarting')
 
 while os.path.isfile('autorestart.txt'):
+    print('updating...')
+    os.system(f'{PYTHON} -m pip install --upgrade pip -r requirements.txt')
     print('starting...')
-    os.system('python3 main.py')
+    os.system(f'{PYTHON} main.py')
     print('stopped')
