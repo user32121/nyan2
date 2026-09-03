18155ba7a9ce435188f0903cde9da64a26361dc9
Juniper <pengjupiter5@gmail.com>
2026-09-03 08:12:13 -0500

    add a countdown before restarting for easier ctrl+c

--- run_constantly.py
+++ run_constantly.py
@@ -11,15 +11,19 @@ while os.path.isfile('autorestart.txt'):
     print('updating...')
     res = os.system('git pull')
     if res:
-        print(f"unable to access git, wait a minute ({res})")
+        print(f"unable to access git, wait a minute ({res})...")
         time.sleep(60)
         continue
     res = os.system(
         f'{PYTHON} -m pip install --upgrade pip -r requirements.txt')
     if res:
-        print(f"unable to update packages, wait a minute ({res})")
+        print(f"unable to update packages, wait a minute ({res})...")
         time.sleep(60)
         continue
     print('starting...')
     os.system(f'{PYTHON} main.py')
     print('stopped')
+    print('restarting in')
+    for i in range(3, 0, -1):
+        print(f"{i}...")
+        time.sleep(1)
